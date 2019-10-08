d05d91bfaaf01132aaa2de56ab89ad0c1799728d
OwenTsoi <ttso7897@uni.sydney.edu.au>
2019-10-08 14:32:57 +1100

    Uploading initial gradle project

---
+++ VendingMachine/src/main/java/CashSystem.java
@@ -0,0 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CashSystem {
+    private Map<String,Double> notes =new HashMap<>();
+}
---
+++ VendingMachine/src/main/java/ItemSystem.java
@@ -0,0 +1,2 @@
+public class ItemSystem {
+}
