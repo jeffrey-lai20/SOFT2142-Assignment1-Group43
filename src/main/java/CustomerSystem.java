72390f038aaf9b49abcecfc680daecfedf4ea450
Uyen <uyen.trannn09@gmail.com>
2019-10-09 20:42:51 +1100

    created plan for customer system

--- src/main/java/CustomerSystem.java
+++ src/main/java/CustomerSystem.java
@@ -1,2 +1,31 @@
-public class CustumerSystem {
+import java.util.*;
+import java.io.*;
+
+public class CustomerSystem {
+
+    // Select item ( availability), specify number of items
+    // Can type in product name or a unique code
+    // Can select more than one item in one go.
+    // Can cancel transaction before buying.
+
+    public static void main(String[] args) {
+
+        // Shows items available and number of items available
+        System.out.println("Items Avaliable: ");
+
+
+        // Type in product name or unique code
+            // Do for while loop until user EOF
+        System.out.println("Select Item: ");
+
+        // Confirmation
+        System.out.println("To confirm, items selected are: ");
+
+        // Continue with transaction or cancel
+        System.out.println("Continue with transaction? (Y/N)");
+            // If yes, take out money and minus the stock available
+            // If no, cancel the transaction
+    }
+
+
 }
