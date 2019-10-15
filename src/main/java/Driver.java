b6cb6cb53ef4721548d170071fe4763deebd71ea
Ramil <rsri3941@uni.sydney.edu.au>
2019-10-15 15:23:07 +1100

    inventory config file

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,9 +1,9 @@
 public class Driver {
     public static void main(String[] args) {
+        // System.out.println(System.getProperty("user.dir"));
         Inventory i = new Inventory();
-        System.out.println("Inventory size = " + i.getQuantity());
-        Item mountFranklin = new ItemImpl("Mount Franklin", 2.5, Item.TYPE.DRINKS);
-        i.addItem(mountFranklin);
-        System.out.println("Inventory size = " + i.getQuantity());
+        // Item mountFranklin = new ItemImpl("Mount Franklin", 2.5, Item.TYPE.DRINKS);
+        // i.addItem(mountFranklin);
+        // System.out.println("Inventory size = " + i.getQuantity());
     }
 }
--- src/main/java/Inventory.java
+++ src/main/java/Inventory.java
@@ -1,20 +1,39 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 
 public class Inventory {
-    final static Integer MAX = 20;
+    final static Integer MAX = 40;
     private ArrayList<Item> items;
     private Integer quantity;
+    private File configFile;
+    private BufferedReader reader;
     
     public Inventory() {
         this.items = new ArrayList<Item>(MAX);
         this.quantity = 0;
+        try {
+            this.configFile = new File(System.getProperty("user.dir") + "/src/main/java/inventory-data.txt");
+            this.reader = new BufferedReader(new FileReader(this.configFile));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] productInfo = line.split(",", 3);
+                Item i = new ItemImpl(productInfo[0], Double.valueOf(productInfo[1]), Item.TYPE.valueOf(productInfo[2])); 
+                this.items.add(i);
+            }
+        } catch (Exception e) {
+            System.out.println("Inventory configuration file missing or corrupted");
+        }
+
     }
 
     public void addItem(Item item) {
-        if (this.quantity <= 20) {
+        if (this.quantity < MAX) {
             this.items.add(item);
             this.quantity++;
             System.out.println(item.getName() + " added successfully.");
+            System.out.println("Inventory Size = " + this.quantity + "/" + MAX);
         } else {
             System.out.println("Inventory Full. Cannot add a new item");
         }
