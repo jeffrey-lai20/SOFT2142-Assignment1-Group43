599f101385f2579b50a8533d0a256fcacebe3c80
Uyen <uyen.trannn09@gmail.com>
2019-10-18 12:43:58 +1100

    added method to check avaliability

--- src/main/java/CustomerSystem.java
+++ src/main/java/CustomerSystem.java
@@ -1,46 +1,84 @@
-<<<<<<< HEAD
-public class CustomerSystem {
-=======
 import java.util.*;
 import java.io.*;
 
 public class CustomerSystem {
 
-    // Select item ( availability), specify number of items
-    // Can type in product name or a unique code
-    // Can select more than one item in one go.
-    // Can cancel transaction before buying.
+        // Can type in product name or a unique code
+        // Can select more than one item in one go.
+        // Can cancel transaction before buying.
 
-    public static void main(String[] args) {
+        // Select item ( availability), specify number of items
+        private void itemsAvaliable(ArrayList<Item> items) {
 
-        // Shows items available and number of items available
-        System.out.println("Items Avaliable: ");
-        // Get items from each category and print it
+                // Sort each items into categories and print them
+                ArrayList<Item> drinks = new ArrayList<Item>();
+                ArrayList<Item> chocolates = new ArrayList<Item>();
+                ArrayList<Item> chips = new ArrayList<Item>();
+                ArrayList<Item> lollies = new ArrayList<Item>();
+
+                for (Item i : items) {
+                        if (i.getType() == DRINKS) {
+                                drinks.add(i);
+                        } else if (i.getType() == CHOCOLATES) {
+                                chocolates.add(i);
+                        } else if (i.getType() == CHIPS) {
+                                chips.add(i);
+                        } else if (i.getType() == LOLLIES) {
+                                lollies.add(i);
+                        }
+                }
+
+                System.out.println("Items Avaliable: ");
+
+                System.out.println("Drinks - ");
+                for (Item i : drinks) {
+                        System.out.println(i.getName());
+                }
+
+                System.out.println("Chocolates - ");
+                for (Item i : chocolates) {
+                        System.out.println(i.getName());
+                }
+
+                System.out.println("Chips - ");
+                for (Item i : chips) {
+                        System.out.println(i.getName());
+                }
+
+                System.out.println("Lollies - ");
+                for (Item i : lollies) {
+                        System.out.println(i.getName());
+                }
+
+        }
+
+        private void enterItem() {
+                // Type in product name or unique code
+                // Do for while loop until user EOF
+                Scanner input = new Scanner(System.in);
+                System.out.println("Enter Item: ");
+
+                String itemSelected = input.nextLine();
+
+                System.out.println("Enter amount: ");
+                int amountSelected = input.nextInt();
 
 
-        // Type in product name or unique code
-            // Do for while loop until user EOF
-        Scanner input = new Scanner(System.in);
-        System.out.println("Select Item: ");
+                System.out.println("You have selected " + item_selected);
 
-        String item_selected = input.nextLine();
+                // Add to cart
+                // Decline --> does not match any product description
+        }
 
-        System.out.println("You have selected " + item_selected);
+        private void confirmation() {
+                // Confirmation
+                System.out.println("To confirm, items selected are: ");
 
-        // Add to cart
-        // Decline --> does not match any product description 
-
-
-
-        // Confirmation
-        System.out.println("To confirm, items selected are: ");
-
-        // Continue with transaction or cancel
-        System.out.println("Continue with transaction? (Y/N)");
-            // If yes, take out money and minus the stock available
-            // If no, cancel the transaction
-    }
+                // Continue with transaction or cancel
+                System.out.println("Continue with transaction? (Y/N)");
+                // If yes, take out money and minus the stock available
+                // If no, cancel the transaction
+        }
 
 
->>>>>>> 543120fde177f93bda9d800345fd42b6dbd4c159
 }
