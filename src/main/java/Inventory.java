9e75bd74ac568a9ba8f4095a4e843d83d63004a5
Ramil <rsri3941@uni.sydney.edu.au>
2019-10-18 13:15:00 +1100

    added quantity to items

--- src/main/java/Inventory.java
+++ src/main/java/Inventory.java
@@ -18,7 +18,7 @@ public class Inventory {
             this.reader = new BufferedReader(new FileReader(this.configFile));
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] productInfo = line.split(",", 3);
+                String[] productInfo = line.split(",", 4);
                 Item i = new ItemImpl(productInfo[0], Double.valueOf(productInfo[1]), Item.TYPE.valueOf(productInfo[2]), Double.valueOf(productInfo[3])); 
                 this.items.add(i);
             }
