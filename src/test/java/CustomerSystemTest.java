28518b88ddf38a47f2ebaf1a6742d4a983bc4182
Uyen <uyen.trannn09@gmail.com>
2019-10-22 13:06:17 +1100

    Added more tests

--- src/test/java/CustomerSystemTest.java
+++ src/test/java/CustomerSystemTest.java
@@ -43,6 +43,43 @@ public class CustomerSystemTest {
 	}
 
 	@Test
+	public void itemAvaliableTest1() {
+		Inventory i = new Inventory();
+		Driver testCustomerSystem = new Driver();
+		CustomerSystem customerSystem = new CustomerSystem(i.getItems());
+
+		ArrayList<Item> items = i.getItems();
+
+		String test2 = ("Here are our items avaliable: \n" +
+				"Drinks - \n" +
+				"Water: "+items.get(0).getQuantity+"\n" +
+				"Soft Drink: "+items.get(1).getQuantity+"\n" +
+				"Juice: "+items.get(2).getQuantity+"\n\n" +
+
+				"Chocolates - \n" +
+				"M&M: "+items.get(3).getQuantity+"\n" +
+				"Bounty: "+items.get(4).getQuantity+"\n" +
+				"Mars: "+items.get(5).getQuantity+"\n" +
+				"Snickers: "+items.get(6).getQuantity+"\n\n" +
+
+				"Chips - \n" +
+				"Original: "+items.get(7).getQuantity+"\n" +
+				"Chicken: "+items.get(8).getQuantity+"\n" +
+				"BBQ: "+items.get(9).getQuantity+"\n" +
+				"Sweet Chilly: "+items.get(10).getQuantity+"\n\n" +
+
+				"Lollies - \n" +
+				"Sour Worms: "+items.get(11).getQuantity+"\n" +
+				"Jellybeans: "+items.get(12).getQuantity+"\n" +
+				"Little Bears: "+items.get(13).getQuantity+"\n" +
+				"Party Mix: "+items.get(14).getQuantity+"\n\n" +
+		);
+
+		assertEquals(test2, customerSystem.itemsAvaliable());
+
+	}
+
+	@Test
 	public void enterItemTest1() {
 		Inventory i = new Inventory();
 		Driver testCustomerSystem = new Driver();
