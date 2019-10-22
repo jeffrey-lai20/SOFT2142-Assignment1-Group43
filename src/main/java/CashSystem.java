c0a3da3e607a9090e079e94428bc6565dde672e5
Ramil <rsri3941@uni.sydney.edu.au>
2019-10-22 16:28:14 +1100

    test fix

--- src/main/java/CashSystem.java
+++ src/main/java/CashSystem.java
@@ -10,7 +10,7 @@ public class CashSystem {
             cashChoiceText();
             String input = user.nextLine();
             if(input.equals("9")){
-                System.out.println("Transaction cancelled.See you next time!\n");
+                System.out.println("Transaction cancelled.See you next time!");
                 return;
             }
             List<Integer> userInput = inputChecker(input);
