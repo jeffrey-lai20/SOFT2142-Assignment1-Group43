f1bf0c96d3513ef0e3655fb6644675b3b136c00a
Uyen <uyen.trannn09@gmail.com>
2019-10-29 13:48:09 +1100

    reset cart after cancel transaction

--- src/main/java/CustomerSystem.java
+++ src/main/java/CustomerSystem.java
@@ -121,6 +121,10 @@ public class CustomerSystem {
                     //calculate cost
                     CashSystem cs = new CashSystem();
                     cs.cashInput(10);
+                    ArrayList<String> emptyCart1 = new ArrayList<>();
+                    ArrayList<Integer> emptyCartQuantity1 = new ArrayList<>();
+                    cart = emptyCart1;
+                    cartQuantity = emptyCartQuantity1;
                     return true;
                 case 2:
                     takeAwayItem(itemSelected,quantitySelected);
@@ -128,10 +132,10 @@ public class CustomerSystem {
                     return true;
                 case 3:
                     System.out.print("Transaction cancelled. Have a good day!\n");
-                    ArrayList<String> emptyCart = new ArrayList<>();
-                    ArrayList<Integer> emptyCartQuantity = new ArrayList<>();
-                    cart = emptyCart;
-                    cartQuantity = emptyCartQuantity;
+                    ArrayList<String> emptyCart2 = new ArrayList<>();
+                    ArrayList<Integer> emptyCartQuantity2 = new ArrayList<>();
+                    cart = emptyCart2;
+                    cartQuantity = emptyCartQuantity2;
                     return false;
                 case 4:
                     viewCart(cart,cartQuantity);
